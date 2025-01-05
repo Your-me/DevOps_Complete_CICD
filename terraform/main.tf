252d871d0988a3a74aa542e10114c4e33c0ff1e1
Your-me <akinmoladunabayomi@gmail.com>
2025-01-05 00:48:17 +0000

    adjusted the main.tf,deploy.yaml file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,40 +1,20 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~>4.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
   backend "s3" {
-    key = "aws/ec2-deploy/terraform.tfstate"
+    bucket = var.bucket_name
+    key    = "aws/ec2-deploy/terraform.tfstate"
+    region = "eu-north-1"      
   }
 }
 
 provider "aws" {
   region = var.region
 }
-
-#tags key should be capitalized
-/*
-resource "aws_instance" "server" {
-  ami = "ami-089146c5626baa6bf"
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.maingroup.id]
-  iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
-  connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ubuntu"
-    private_key = var.private_key
-    timeout = "4m"
-  }
-  tags = {
-    "name" = "DeployWM"
-  }
-}
-*/
-
 
 resource "aws_instance" "server" {
   ami                    = "ami-089146c5626baa6bf"
@@ -56,45 +36,27 @@ resource "aws_instance" "server" {
   }
 }
 
-
 resource "aws_security_group" "maingroup" {
-  egress = [ 
-    {
-        cidr_blocks = ["0.0.0.0/0"]
-        description = ""
-        from_port = 0
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        protocol = "-1"
-        security_groups = []
-        self = false
-        to_port = 0
-    }
-   ]
-   ingress = [ 
-    {
-        cidr_blocks = ["0.0.0.0/0",]
-        description = ""
-        from_port = 22
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        protocol = "tcp"
-        security_groups = []
-        self = false
-        to_port = 22      
-    },
-     {
-        cidr_blocks = ["0.0.0.0/0",]
-        description = ""
-        from_port = 80
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        protocol = "tcp"
-        security_groups = []
-        self = false
-        to_port = 80     
-    }
-    ]
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+  }
 }
 
 resource "aws_iam_instance_profile" "ec2-profile" {
@@ -103,11 +65,11 @@ resource "aws_iam_instance_profile" "ec2-profile" {
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name = var.key_name
+  key_name   = var.key_name
   public_key = var.public_key
 }
 
 output "instance_public_ip" {
-  value = aws_instance.server.public_ip
+  value     = aws_instance.server.public_ip
   sensitive = true
 }
